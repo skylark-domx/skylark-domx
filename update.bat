227478f69d61c1fe6d100bf4c70b5a55b9565fc7
hudaokeji <github@hudaokeji.com>
2021-07-23 08:37:22 +0900

    update

--- update.bat
+++ update.bat
@@ -1,1 +1,1 @@
-spkgm install --force --no-lockfile --flat & spkgm run build & git add --all & git commit -m "update" & git push
+spkgm install --force --no-lockfile & spkgm run build & git add --all & git commit -m "update" & git push
